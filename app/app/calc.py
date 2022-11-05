42eca53cce772dafc776ded789a8d018d33637b1
sagnolkamdem <sagnolkamdem721@gmail.com>
2022-11-05 10:32:51 +0100

    start postgres configuration

---
+++ app/app/calc.py
@@ -0,0 +1,21 @@
+def add(x, y):
+    """
+    The add function adds two numbers together and returns the result.
+
+    param x: Pass the first value to be added
+    param y: Provide a default value for the y parameter
+    return: The sum of x and y
+    doc-author: Trelent
+    """
+    return x + y
+
+
+def substract(x, y):
+    """
+    The substract function subtracts two numbers.
+    param x: Pass the first value to be substracted
+    param y: Substract the y value from x
+    return: The difference of x and y
+    doc-author: Trelent
+    """
+    return x - y
